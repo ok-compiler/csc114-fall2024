b4fce781f2399274eba40b0a202ed7ed47e41ad4
owebst <websterowen15@gmail.com>
2024-10-06 21:38:13 -0400

    add module4 hw

---
+++ final/main.cpp
@@ -0,0 +1,14 @@
+// jrpg game where you attack enemies with hp and have items, powerups, etc
+
+#include <iostream>
+using namespace std;
+
+string item[2][3] = {
+{"name": "yea", "type": "idk", "rarity": "kinda"},
+{"name": "no", "type": "idk", "rarity": "sorta"}
+{
+}
+
+int main() {
+	return 0;
+}
