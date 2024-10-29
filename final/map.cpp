7d51e2e86f6c033e17b9a3a5d348c891403a4888
owebst <websterowen15@gmail.com>
2024-10-29 17:54:24 -0400

    work on comments in code

--- final/map.cpp
+++ final/map.cpp
@@ -11,13 +11,18 @@ using namespace std::this_thread;
 using namespace std::chrono_literals;
 using std::chrono::system_clock;
 
+// map/score values
+int score = 0; // player score, increases when line clears
 
-int score = 0;
-const int mapX = 12;
-const int mapY = 22;
-char map[mapY][mapX];
-int color[mapY][mapX];
-int nextPiecePrint[7][2][4] = { 
+const int mapX = 12; // map size (length)
+const int mapY = 22; // map size (width)
+
+char map[mapY][mapX]; //main map, visible on each piece update
+int color[mapY][mapX]; // color map, used to know which color is assigned to which character
+// ^^ this should have gone into a 3d array on map instead LOL
+
+// values for displaying the next piece
+int nextPiecePrint[7][2][4] = {  
 {{'@', '@', '@', '@'}, {' ', ' ', ' ', ' '}}, // I piece
 {{'@', '@', '@', ' '}, {'@', ' ', ' ', ' '}}, // L piece
 {{'@', '@', '@', ' '}, {' ', '@', ' ', ' '}}, // T piece
@@ -28,13 +33,14 @@ int nextPiecePrint[7][2][4] = {
 };
 
 
-void mapInit() {
-    for (int y = 0; y < mapY; y++) {
-        for (int x = 0; x < mapX; x++) {
-            if (x == mapX - 1 || x == 0 || y == mapY - 1 || y == 0) {
+// initializes map
+void mapInit() { 
+    for (int y = 0; y < mapY; y++) { // each mapY value (y)
+        for (int x = 0; x < mapX; x++) { // each mapX value (x)
+            if (x == mapX - 1 || x == 0 || y == mapY - 1 || y == 0) { // if border, set to #
                 map[y][x] = '#';
                 color[y][x] = 7;
-            } else if (map[y][x] != '#') {
+            } else if (map[y][x] != '#') { // if not a border, set to blank space
                 map[y][x] = ' ';
                 color[y][x] = 8;
             }
@@ -42,31 +48,33 @@ void mapInit() {
     }
 }
 
+// prints based on input (a and b)
 void checkColor(int a, char b) {
-    string c = string(&b, &b + 1);
-    switch (a) {
-        case 0: cout << "\033[106m" + c + "\033[0m"; break;
-        case 1: cout << "\033[44m" + c + "\033[0m"; break;
-        case 2: cout << "\033[45m" + c + "\033[0m"; break;
-        case 3: cout << "\033[43m" + c + "\033[0m"; break;
-        case 4: cout << "\033[102m" + c + "\033[0m"; break;
-        case 5: cout << "\033[41m" + c + "\033[0m"; break;
-        case 6: cout << "\033[103m" + c + "\033[0m"; break;
-        case 7: cout << "\033[107m" + c + "\033[0m"; break;
-        case 8: cout << "\033[100m" + c + "\033[0m"; break;
+    string c = string(&b, &b + 1); // converts b to string
+    switch (a) { // checks color value (a, usually pieceValue)
+        case 0: cout << "\033[106m" + c + "\033[0m"; break; // outputs char b as cyan
+        case 1: cout << "\033[44m" + c + "\033[0m"; break; // outputs char b as orange
+        case 2: cout << "\033[45m" + c + "\033[0m"; break; // outputs char b as purple
+        case 3: cout << "\033[43m" + c + "\033[0m"; break; // outputs char b as blue
+        case 4: cout << "\033[102m" + c + "\033[0m"; break; // outputs char b as lime green
+        case 5: cout << "\033[41m" + c + "\033[0m"; break; // outputs char b as red
+        case 6: cout << "\033[103m" + c + "\033[0m"; break; // outputs char b as yellow
+        case 7: cout << "\033[107m" + c + "\033[0m"; break; // outputs char b as white
+        case 8: cout << "\033[100m" + c + "\033[0m"; break; // outputs char b as light gray
         default: cerr << "error" << endl;
     }
 }
 
-void printMap() {
-    lock_guard<mutex> lock(mtx);
-    system("clear");
-    system("stty cooked");
+// all the logic for printing map
+void printMap() { 
+    lock_guard<mutex> lock(mtx); // set mutex for threads so info stays accurate
+    system("clear"); // clear console
+    system("stty cooked"); // for proper outputs
     for (int y = 0; y < mapY; y++) {
         for (int x = 0; x < mapX; x++) {
-            checkColor(color[y][x], map[y][x]);
-            if (x == mapX - 1) {
-            	switch (y){ // displays on right side of map
+            checkColor(color[y][x], map[y][x]); // print map value with color array (same values)
+            if (x == mapX - 1) { // checks if it's the rightmost char
+            	switch (y){ // runs all displays on right side of map
             		case 0:
             			for (int i = 0; i < 13; i++) {
             				checkColor(7, '#');
@@ -92,7 +100,7 @@ void printMap() {
     	     	   	case 4: 
     	     	   		cout << "\t";
     	     	   		for (int i = 0; i < 4; i++) {
-    	     	   			if (nextPiecePrint[nextPiece][0][i] == '@') checkColor(nextPiece, nextPiecePrint[nextPiece][0][i]);
+    	     	   			if (nextPiecePrint[nextPiece][0][i] == '@') checkColor(nextPiece, nextPiecePrint[nextPiece][0][i]); //prints next piece from nextPiecePrint array
     	     	  			else cout << string(&nextPiecePrint[nextPiece][0][i], &nextPiecePrint[nextPiece][0][i] + 1);
     	     	   		}
     	     	   		cout << "    ";
@@ -198,10 +206,11 @@ void printMap() {
             }
     	}
     }
-    //cout << "Controls:\n~: Quit\ta: move left\ts: move down\td: move right\tw: rotate clockwise\te: rotate counterclockwise";
-    system("stty raw");
+    system("stty raw"); //back to raw for player inputs
 }
 
+
+// check for line clears
 void lineClear() {
     int collisionCheck_clear = 0;
     int lineClears = 0;
@@ -209,11 +218,11 @@ void lineClear() {
         for (int x = 0; x < mapX; x++) {
             if (map[y][x] == '#' && y != 0 && y != mapY - 1) collisionCheck_clear++;
         }
-        if (collisionCheck_clear == mapX) {
-            lineClears++;
+        if (collisionCheck_clear == mapX) { //if all values across a line are '#', line cleared
+            lineClears++; // adds to lineClears value
             for (int row = y; row > 1; row--) {
                 for (int col = 1; col < mapX - 1; col++) {
-                    map[row][col] = map[row - 1][col];
+                    map[row][col] = map[row - 1][col]; //shifts everything down
                     color[row][col] = color[row - 1][col];
                 }
             }
@@ -224,7 +233,7 @@ void lineClear() {
         }
         collisionCheck_clear = 0;
     }
-    switch (lineClears) {
+    switch (lineClears) { // sets score based on amount of lines cleared
         case 1: score += 40; break;
         case 2: score += 100; break;
         case 3: score += 300; break;
--- final/piece.cpp
+++ final/piece.cpp
@@ -4,39 +4,60 @@
 #include <stdio.h> 
 #include <string>
 using namespace std;
+
+// for threads so they dont mess with stuff at the same time lol
 #include <mutex> 
 
+
+// clock stuff for gravity
 #include <chrono>
 #include <thread>
 using namespace std::this_thread;
 using namespace std::chrono_literals;
 using std::chrono::system_clock;
 
-int pieceX[4] = {4, 5, 6, 7};
+
+// initial piece positions
+int pieceX[4] = {4, 5, 6, 7}; 
 int pieceY[4] = {1, 1, 1, 1};
+
+// i dont even remember tbh, old code was cluttered LOL
+// all are temporary new values for pieces, used when checking collision
+// if collision is good, sets the value to pieceX and pieceY respectively
+// incredibly inefficient
 int newpieceX_spawn[4] = {4, 5, 6, 7};
 int newpieceY_spawn[4] = {1, 1, 1, 1};
 int newpieceX_spawn_copy[4] = {4, 5, 6, 7};
 int newpieceY_spawn_copy[4] = {1, 1, 1, 1};
 int newpieceX[4], newpieceY[4];
 
-int pieceValue = 0;
-int nextPiece = 1;
-int pieceBank[7] = {0, -1, 2, 3, 4, 5, 6};
-bool spawnNew = true;
-int collisionCheck = 0;
-char wasd;
+int pieceValue = 0; // color value assigned to current piece
+int nextPiece = 1; // color value assigned to next piece
 
+// piece bank, used to make sure all pieces are used
+// after pieces are randomly selected, their value becomes -1
+// after all pieces are -1, bank resets
+// same logic as all new tetris games, just less efficient LOL
+int pieceBank[7] = {0, -1, 2, 3, 4, 5, 6}; 
+bool spawnNew = true; // used for piece generation loop
+int collisionCheck = 0; 
+
+
+char wasd; // player input
+
+
+// rotates piece position
 void rotatePiece(bool clockwise) {
+
     int collisionCheck_rotation = 0;
-    int pivotX = pieceX[1];
+    int pivotX = pieceX[1]; // pivot values
     int pivotY = pieceY[1];
-    int newpieceX_rotation[4];
+    int newpieceX_rotation[4]; // new piece values
     int newpieceY_rotation[4];
 
     for (int i = 0; i < 4; i++) {
         if (clockwise) {
-            newpieceX_rotation[i] = pivotX - (pieceY[i] - pivotY);
+            newpieceX_rotation[i] = pivotX - (pieceY[i] - pivotY);  // rotates by flipping x and y values and whatnot
             newpieceY_rotation[i] = pivotY + (pieceX[i] - pivotX);
         } else {
             newpieceX_rotation[i] = pivotX + (pieceY[i] - pivotY);
@@ -46,11 +67,11 @@ void rotatePiece(bool clockwise) {
 
     for (int i = 0; i < 4; i++) {
         if (map[newpieceY_rotation[i]][newpieceX_rotation[i]] != '#') {
-            collisionCheck_rotation++;
+            collisionCheck_rotation++; // checks collision
         }
     }
 
-    if (collisionCheck_rotation >= 4) {
+    if (collisionCheck_rotation >= 4) { // if all pieces are good, update piece
         for (int i = 0; i < 4; i++) {
             pieceX[i] = newpieceX_rotation[i];
             pieceY[i] = newpieceY_rotation[i];
@@ -58,6 +79,8 @@ void rotatePiece(bool clockwise) {
     }
 }
 
+
+// game over screen, self explanatory
 void gameOver() {
 	//system("clear");
 	system("stty cooked");
@@ -81,8 +104,10 @@ void gameOver() {
 	system("stty cooked");
 }
 
+
+// initializes next random piece
 void initPiece() {
-	while (spawnNew) {
+	while (spawnNew) { // becomes false once open piece from pieceBank is selected
 		nextPiece = rand() % 7; //new random int
 		if (nextPiece != pieceBank[nextPiece]) spawnNew = true; // if used, restart
 		else  {
@@ -101,11 +126,13 @@ void initPiece() {
 	spawnNew = true;
 }
 
+
+// sets pieceX and pieceY based on piece value selected by initPiece();
 void spawnNewPiece() {
 	pieceValue = nextPiece;
 	initPiece();
 	//lock_guard<mutex> lock(mtx);
-	switch(pieceValue) { // dont need i piece since its default 
+	switch(pieceValue) { // dont need the I piece since its default 
 		case 1: // L piece (blue)
    			newpieceX_spawn[0]++;
 			newpieceY_spawn[0]++;
@@ -142,7 +169,7 @@ void spawnNewPiece() {
     		break;
     		default: cerr << "error" << endl;
     	}
-   	for(int i = 0; i < 4; i++) {
+   	for(int i = 0; i < 4; i++) { // sets all values
 		pieceX[i] = newpieceX_spawn[i];
 		pieceY[i] = newpieceY_spawn[i];
 		newpieceY[i] = pieceY[i];
@@ -153,8 +180,10 @@ void spawnNewPiece() {
 	}
 }
 
+// updates piece position based on player input
+// needs its own thread due to while(true) loop
 void updatePiece(int init) {
-	if (init == 0) {
+	if (init == 0) { // if first being ran, set initial piece values in map
 		{
 			lock_guard<mutex> lock(mtx);
 			for(int i = 0; i < 4; i++) {
@@ -166,9 +195,9 @@ void updatePiece(int init) {
 	}
 	else {
 	while(true) {
-		system("stty raw");
-		{
-			lock_guard<mutex> lock(mtx);
+		system("stty raw"); // used to collect inputs without pressing enter
+		{ // these brackets specify what mutex can control
+			lock_guard<mutex> lock(mtx); // makes sure other threads dont interfere
 			for(int i = 0; i < 4; i++) {
 				newpieceX[i] = pieceX[i];
 				newpieceY[i] = pieceY[i];
@@ -186,12 +215,12 @@ void updatePiece(int init) {
 			break;
 			case 'a': 
 				for(int i = 0; i < 4; i++) {
-					newpieceX[i]--;
+					newpieceX[i]--; // moves piece left
 					if (map[newpieceY[i]][newpieceX[i]] != '#') { 
-						collisionCheck++;
+						collisionCheck++; // checks collision
 					}
 				}
-				if (collisionCheck == 4) {
+				if (collisionCheck == 4) { // if all values satisfy collision, proceed
 						//lock_guard<mutex> lock(mtx);
 						mapInit();
 						for(int i = 0; i < 4; i++) {
@@ -205,12 +234,12 @@ void updatePiece(int init) {
 			break;
 			case 's': 
 				for(int i = 0; i < 4; i++) {
-					newpieceY[i]++;
+					newpieceY[i]++; // moves piece down
 					if (map[newpieceY[i]][newpieceX[i]] != '#') { 
-						collisionCheck++;
+						collisionCheck++; // checks collision
 					}
 				}
-				if (collisionCheck == 4) {
+				if (collisionCheck == 4) { // if all values satisfy collision, proceed
 						//lock_guard<mutex> lock(mtx);
 						mapInit();
 						for(int i = 0; i < 4; i++) {
@@ -224,12 +253,12 @@ void updatePiece(int init) {
 			break;
 			case 'd': 
 				for(int i = 0; i < 4; i++) {
-					newpieceX[i]++;
+					newpieceX[i]++; // moves piece right
 					if (map[newpieceY[i]][newpieceX[i]] != '#') { 
-						collisionCheck++;
+						collisionCheck++; // checks collision
 					}
 				}
-				if (collisionCheck == 4) {
+				if (collisionCheck == 4) { // if all values satisfy collision, proceed
 					//lock_guard<mutex> lock(mtx);
 					mapInit();
 					for(int i = 0; i < 4; i++) {
@@ -242,17 +271,18 @@ void updatePiece(int init) {
 				}
 			break;
 			case '~':
-				exit(1);
+				exit(1); // exits code when ~ is pressed
 			break;
 		}
-		collisionCheck = 0;
+		collisionCheck = 0; // resets collisionCheck at the end of each input
 		system("stty cooked");
 	}
 	}
 }
 
+// gravity function
 void updatePos() {	
-	srand(time(0));
+	srand(time(0)); // random seed based on time
 	int collisionCheck_delay;
 	while(true) {
 	{
